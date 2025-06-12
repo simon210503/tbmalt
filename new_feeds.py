77df1bc3ca927aab17ff9ebbb3b0a714f5d344e8
Simon Tönjes <stoenjes@uni-bremen.de>
2025-06-12 11:51:30 +0200

    Gamma + Cutoff added + bug fix

--- new_feeds.py
+++ new_feeds.py
@@ -48,13 +48,17 @@ class xTBRepulsive(Feed):
             c[1] := Z_B^eff
             c[2] := α_A
             c[3] := α_B
+            c[4] := k_f
+        cutoff: Cutoff radius for the repulsive beyond which interactions
+            are assumed to be zero.
     """
 
     def __init__(
-            self, coefficients: Parameter):
+            self, coefficients: Parameter, cutoff: Tensor):
 
         super().__init__()
         self.coefficients = coefficients
+        self.cutoff = cutoff
 
     def forward(self, distances: Tensor) -> Tensor:
         """Evaluate the repulsive interaction at the specified distance(s).
@@ -73,9 +77,10 @@ class xTBRepulsive(Feed):
         z2 = c[1]
         a1 = c[2]
         a2 = c[3]
-        kf = 1.5
+        kf = c[4]
+        mask = distances < self.cutoff
 
-        results = z1 * z2 / distances * torch.exp(-torch.sqrt(a1 * a2) * distances**kf)
+        results[mask] = z1 * z2 / distances[mask] * torch.exp(-torch.sqrt(a1 * a2) * distances[mask]**kf)
 
         return results
     
@@ -93,13 +98,16 @@ class PTBPRepulsive(Feed):
             c[1] := Z_B^eff
             c[2] := α_A
             c[3] := α_B
+        cutoff: Cutoff radius for the repulsive beyond which interactions
+            are assumed to be zero.
     """
 
     def __init__(
-            self, coefficients: Parameter):
+            self, coefficients: Parameter, cutoff: Tensor):
 
         super().__init__()
         self.coefficients = coefficients
+        self.cutoff = cutoff
 
     def forward(self, distances: Tensor) -> Tensor:
         """Evaluate the repulsive interaction at the specified distance(s).
@@ -119,12 +127,94 @@ class PTBPRepulsive(Feed):
         a1 = c[2]
         a2 = c[3]
         gamma = 1 / torch.sqrt(a1**2 + a2**2)
+        mask = distances < self.cutoff
 
-        results = z1 * z2 / distances * (1 - torch.erf(gamma * distances))
+        results[mask] = z1 * z2 / distances[mask] * (1 - torch.erf(gamma * distances[mask]))
+
+        return results
+    
+class DFTBGammaRepulsive(Feed):
+     
+    """Repulsive in form of the DFTB-Gamma.
+
+    
+
+    Arguments:
+        coefficients: List containing import parameter
+            c[0] := Z_A^eff
+            c[1] := Z_B^eff
+            c[2] := α_A
+            c[3] := α_B
+        cutoff: Cutoff radius for the repulsive beyond which interactions
+            are assumed to be zero.
+    """
+
+    def __init__(
+            self, coefficients: Parameter, cutoff: Tensor):
+
+        super().__init__()
+        self.coefficients = coefficients
+        self.cutoff = cutoff
+
+    def forward(self, distances: Tensor) -> Tensor:
+        """Evaluate the repulsive interaction at the specified distance(s).
+
+        Arguments:
+            distances: Distance(s) at which the repulsive term is to be
+                evaluated.
+
+        Returns:
+            repulsive: Repulsive interaction energy as evaluated at the
+                specified distances.
+        """
+        results = torch.zeros_like(distances)
+        c = self.coefficients
+        z1 = c[0]
+        z2 = c[1]
+        a1 = c[2]
+        a2 = c[3]
+        mask = distances < self.cutoff
+
+        if a1 == a2:
+            results[mask] = self._equal_gamma(distances[mask], a1)
+        
+        else:
+            results[mask] = self._unequal_gamma(distances[mask], a1, a2)
+
+
+        return results * z1 * z2
+    
+    def _Gamma(self, a, b, R):
+        zaehler1 = b**4 * a
+        nenner1 = 2 * (a**2 - b**2)**2
+        zaehler2 = b**6 - 3 * b**4 * a**2
+        nenner2 = (a**2 - b**2)**3 * R
+        result = zaehler1 / nenner1 - zaehler2 / nenner2
+        return result
+    
+    def _equal_gamma(self, distances, a1):
+        term1 = 1 / distances
+        term2 = 11 * a1 / 16
+        term3 = 3 * a1**2 * distances / 16
+        term4 = a1**3 * distances**2 / 48
+
+        poly = term1 + term2 + term3 + term4
+
+        exp = torch.exp(-a1 * distances)
+        results = 1 / distances - exp * poly
+        return results
+    
+    def _unequal_gamma(self, distances, a1, a2):
+        exp1 = torch.exp(-a1 * distances)
+        exp2 = torch.exp(-a2 * distances)
+        term1 = 1 / distances
+        term2 = exp1 * self._Gamma(a1, a2, distances)
+        term3 = exp2 * self._Gamma(a2, a1, distances)
+        results = term1 - term2 - term3
 
         return results
 
-def pairwise_repulsive(Geometry, alpha, Z, Repulsive):
+def pairwise_repulsive(Geometry, alpha, Z, Repulsive, cutoff):
     """
     Delivers input for PairwiseRepulsiveEnergyFeed
 
@@ -134,7 +224,8 @@ def pairwise_repulsive(Geometry, alpha, Z, Repulsive):
                 (with atomic number as key)
         Z: Dictionary contaning element specific effective charge
                 (with atomic number as key)
-        Repulsive: Type of Repulsive to be used. The following options exist:
+        Repulsive: Type of Repulsive to be used. It works for the following
+                    options:
             - xTBRepulsive
             - PTBPRepulsive
 
@@ -148,11 +239,21 @@ def pairwise_repulsive(Geometry, alpha, Z, Repulsive):
     Dict = ModuleDict({})
     for species_pair, _, _ in atomic_pair_distances(
         Geometry, True, True):
+        cond1 = species_pair[0].item() <= 2
+        cond2 = species_pair[1].item() <= 2
+        if cond1 and cond2:
+            kb = 1
+        else:
+            kb = 1.5
         Dict[str((species_pair[0].item(), species_pair[1].item()))
              ] = Repulsive([Z[species_pair[0].item()],
-                               Z[species_pair[1].item()],
-                               alpha[species_pair[0].item()],
-                               alpha[species_pair[1].item()]])
+                            Z[species_pair[1].item()],
+                            alpha[species_pair[0].item()],
+                            alpha[species_pair[1].item()],
+                            kb], 
+                            cutoff[str((species_pair[0].item(),
+                                         species_pair[1].item()))
+             ])
     return Dict
 
 
@@ -167,23 +268,39 @@ if __name__ == '__main__':
         1: Parameter(Tensor([1.0]),requires_grad = True),
         8: Parameter(Tensor([8.0]),requires_grad = True)
     }
+
+    cutoff = {}
     
     H2O_geo = Geometry(torch.tensor([8, 1, 1]), 
                torch.tensor([[0.0, -1.0, 0.0],
                              [0.0, 0.0, 0.78306400000],
-                             [0.0, 0.0, -0.78306400000]], requires_grad=True),
+                             [0.0, 0.0, -0.78306400000]], requires_grad=False),
                units='angstrom'
                )
+    
+    for species_pair, _, _ in atomic_pair_distances(
+        H2O_geo, True, True):
+        cutoff[str((species_pair[0].item(), species_pair[1].item()))
+               ]= Tensor([5.0])
 
-    xTB_pair_repulsive = pairwise_repulsive(H2O_geo, alpha, Z, xTBRepulsive)
+    xTB_pair_repulsive = pairwise_repulsive(H2O_geo, alpha, Z, xTBRepulsive, cutoff)
 
     xTB_total_repulsive = PairwiseRepulsiveEnergyFeed(xTB_pair_repulsive)
 
     print(xTB_total_repulsive.forward(H2O_geo))
 
-    PTBP_pair_repulsive = pairwise_repulsive(H2O_geo, alpha, Z, PTBPRepulsive)
+    #PTBP_pair_repulsive = pairwise_repulsive(H2O_geo, alpha, Z, PTBPRepulsive)
 
-    PTBP_total_repulsive = PairwiseRepulsiveEnergyFeed(PTBP_pair_repulsive)
+    #PTBP_total_repulsive = PairwiseRepulsiveEnergyFeed(PTBP_pair_repulsive)
     
-    print(PTBP_total_repulsive.forward(H2O_geo))
+    #print(PTBP_total_repulsive.forward(H2O_geo))
+
+    Gamma = DFTBGammaRepulsive([Parameter(Tensor([0.2236]),requires_grad = True),
+                                Parameter(Tensor([7.3724]),requires_grad = True),
+                                Parameter(Tensor([0.2580]),requires_grad = True),
+                                Parameter(Tensor([0.0065]),requires_grad = True)], 
+                                3.0)
+    
+    #print(Gamma.forward(Tensor(torch.arange(0, 5, 0.1))))
+    
     
